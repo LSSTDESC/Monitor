803670b517f05543f62c62618036cff45b398c93
Bryce Kalmbach <jbkalmbach@gmail.com>
2016-01-31 13:13:46 -0800

    Skeleton in place.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# SLMonitor
-Extract light curves for all time-variable lens candidates
+# Monitor
+Extract light curves for all time-variable candidates.
+
+## Demo
+
+Placeholder:
+```
+    python Monitor/monitor.py
+```
+
+## People
+
+* [Bryce Kalmbach](https://github.com/DarkEnergyScienceCollaboration/Monitor/issues/new?body=@jbkalmbach) (UW)
+* [Phil Marshall](https://github.com/DarkEnergyScienceCollaboration/Monitor/issues/new?body=@drphilmarshall) (SLAC)
+
+## License etc
+
+This is open source software, available under the BSD license. If you are interested in this project, please do drop us a line via the hyperlinked contact names above, or by [writing us an issue](https://github.com/DarkEnergyScienceCollaboration/Monitor/issues/new).
---
+++ monitor/monitor.py
@@ -0,0 +1,32 @@
+# ==============================================================================
+# License info here?
+# ==============================================================================
+
+import monitor
+
+# ==============================================================================
+
+class Monitor(object):
+    '''
+    Simple class for extracting DM forced photometry light curves.
+    '''
+    def __init__(self):
+        return
+
+    def read(self,photfile):
+        print("No IO enabled yet.")
+        return
+
+    def run(self,algorithm=None):
+        if algorithm is None:
+            print("No algorithms coded yet.")
+        return
+
+# ==============================================================================
+# Need better tests...
+
+if __name__ == '__main__':
+
+    newMonitor = monitor.Monitor()
+
+# ==============================================================================
