033342ebd7a2b6ef05b818ab5fbc160238b7e87d
Jim Chiang <jchiang@slac.stanford.edu>
2016-02-02 09:01:50 -0800

    move module directory; add unit test code; setup files;update README

---
+++ tests/test_Monitor.py
@@ -0,0 +1,24 @@
+"""
+Unit tests for Monitor class.
+"""
+import os
+import subprocess
+import unittest
+import desc.monitor
+
+class MonitorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.monitor = desc.monitor.Monitor()
+        self.photfile = 'light_curve.txt'
+        subprocess.call('touch %s' % self.photfile, shell=True)
+    def tearDown(self):
+        os.remove(self.photfile)
+        del self.monitor
+    def test_read(self):
+        self.monitor.read(self.photfile)
+    def test_run(self):
+        self.monitor.read(self.photfile)
+        self.monitor.run()
+
+if __name__ == '__main__':
+    unittest.main()
