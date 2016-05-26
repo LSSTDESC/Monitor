141d062257c7669d739225c3ef0513b4aa168e3f
R. Biswas <rbiswas4@gmail.com>
2016-05-25 17:07:17 -0700

    modified:   python/desc/monitor/dataRepo.py

--- python/desc/monitor/dataRepo.py
+++ python/desc/monitor/dataRepo.py
@@ -4,9 +4,10 @@ from sqlalchemy import create_engine
 
 
 def forcedSourcesFromRepo(tract, visit, filterName, butler, dataFrame=None):
+
     dataID = dict(tract=0, visit=visit, filter=filterName)
-    t = butler.get('forced_src', dataId=dataId, immediate=True)
-    df  = pd.dataFrame()
+    t = butler.get('forced_src', dataId=dataID, immediate=True)
+    df = pd.dataFrame()
     for col in t.schema.getNames():
         df[col] = t[t.schema[col].asKey()]
     for key in dataID:
@@ -27,10 +28,26 @@ def ingestDataFrame(dataBase, exists='append'):
 class ForcedSources(object):
 
     def __init__(self, repo, database, outputdir):
-
+        """
+        """
         self.repo = repo
         self.database = database
         self.butler = dp.Butler(repo, outputRoot=output) 
         engine = create_engine('sqlite:///' + database)
 
+    def ingestForcedSources(self, dataID):
 
+
+    def forcedSources(self, dataID, dataFrame=None):
+    
+        # dataID = dict(tract=0, visit=visit, filter=filterName)
+        t = butler.get('forced_src', dataId=dataID, immediate=True)
+        df = pd.dataFrame()
+        for col in t.schema.getNames():
+            df[col] = t[t.schema[col].asKey()]
+        for key in dataID:
+            df[key] = dataID[key]
+        if not dataFrame is None:
+            return pd.concat(df, dataFrame)
+        else:
+            return df
