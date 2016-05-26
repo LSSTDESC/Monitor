7b39b88e9fdb8bb2c4099b3f877a372ad831269a
R. Biswas <rbiswas4@gmail.com>
2016-05-25 18:23:02 -0700

    renamed:    __init__.py -> init.py.swp 	modified:   monitor/__init__.py 	modified:   monitor/dbConnection.py 	modified:   ../../tests/test_Monitor.py

--- python/desc/monitor/__init__.py
+++ python/desc/monitor/__init__.py
@@ -1,1 +1,2 @@
-from monitor import *
+from __future__ import absolute_import
+from . monitor import *
--- python/desc/monitor/dbConnection.py
+++ python/desc/monitor/dbConnection.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-
 import numpy as np
 from lsst.sims.catalogs.generation.db import DBObject
 
