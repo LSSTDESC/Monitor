c40b8d3b4d11d3a0e4cf3a2de35963c3ca32293d
Bryce Kalmbach <jbkalmbach@gmail.com>
2016-11-09 16:53:14 -0500

    Added project match in database query

--- python/desc/monitor/dbConnection.py
+++ python/desc/monitor/dbConnection.py
@@ -6,10 +6,12 @@ __all__ = ['dbInterface']
 
 class dbInterface(object):
 
-    def __init__(self, database, host, port, driver):
+    def __init__(self, database, host, port, driver,
+                 project='Twinkles Run1.1'):
 
         self._dbo = DBObject(database=database, host=host, port=port,
                              driver=driver)
+        self.project = project
 
     def forcedSourceFromId(self, objectId):
 
@@ -20,7 +22,8 @@ class dbInterface(object):
                           ('flags', np.int)])
         query = """select objectId, ccdVisitId, psFlux, psFlux_Sigma, flags
                    from ForcedSource
-                   where objectId = %i""" % objectId
+                   where objectId = %i
+                   and project = %s""" % (objectId, self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
@@ -32,7 +35,8 @@ class dbInterface(object):
 
         query = """select visitId, filterName, obsStart
                    from CcdVisit
-                   where ccdVisitId = %i""" % visitId
+                   where ccdVisitId = %i
+                   and project = %s""" % (visitId, self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
@@ -54,7 +58,9 @@ class dbInterface(object):
                 from ForcedSource
                 inner join CcdVisit on
                        ForcedSource.ccdVisitId = CcdVisit.ccdVisitId
-                       and ForcedSource.objectId = %i""" % objectId
+                       and ForcedSource.objectId = %i
+                       and ForcedSource.project = %s""" % (objectId,
+                                                           self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
@@ -66,7 +72,8 @@ class dbInterface(object):
                           ('dec', np.float)])
         query = """select objectId, parentObjectId, psRa, psDecl
                    from Object
-                   where objectId = %i""" % objectId
+                   where objectId = %i
+                   and project = %s""" % (objectId, self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
@@ -79,9 +86,32 @@ class dbInterface(object):
         query = """select objectId, parentObjectId, psRa, psDecl
                    from Object
                    where (psRa > %f) and (psRa < %f)
-                   and (psDecl > %f) and (psDecl < %f)""" % (ra - tol,
-                                                             ra + tol,
-                                                             dec - tol,
-                                                             dec + tol)
+                   and (psDecl > %f) and (psDecl < %f)
+                   and project = %s""" % (ra - tol, ra + tol,
+                                          dec - tol, dec + tol, self.project)
+        results = self._dbo.execute_arbitrary(query, dtype=dtype)
+        return results
+
+    def get_all_visit_info(self):
+
+        dtype = np.dtype([('visit_id', np.int),
+                          ('filter', str, 300),
+                          ('obs_start', str, 300)])
+
+        query = """select visitId, filterName, obsStart
+                   from CcdVisit where project = %s""" % self.project
+        results = self._dbo.execute_arbitrary(query, dtype=dtype)
+
+        return results
+
+    def get_all_objects_in_visit(self, ccd_visit_id):
+
+        dtype = np.dtype([('psf_flux', np.float),
+                          ('psf_flux_err', np.float),
+                          ('flags', np.int)])
+        query = """select psFlux, psFlux_Sigma, flags
+                   from ForcedSource
+                   where ccd_visit_id = %i
+                   and project = %s""" % (ccd_visit_id, self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
