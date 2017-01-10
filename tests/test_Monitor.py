938ec613d46a2020d083cb58d946a816fc11e5e2
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-01-10 15:42:06 -0800

    Updated barebones monitor test.

--- tests/test_Monitor.py
+++ tests/test_Monitor.py
@@ -9,17 +9,17 @@ import desc.monitor
 
 class MonitorTestCase(unittest.TestCase):
     def setUp(self):
-        self.monitor = desc.monitor.Monitor()
+        self.dbConn = desc.monitor.dbInterface(database='test_cache.db',
+                                               host=None, port=None,
+                                               driver='sqlite')
+        self.monitor = desc.monitor.Monitor(self.dbConn)
         self.photfile = 'light_curve.txt'
         subprocess.call('touch %s' % self.photfile, shell=True)
     def tearDown(self):
         os.remove(self.photfile)
         del self.monitor
-    def test_read(self):
-        self.monitor.read(self.photfile)
-    def test_run(self):
-        self.monitor.read(self.photfile)
-        self.monitor.run()
+    def test_db_load(self):
+        self.assertEqual(self.monitor.num_visits, 1)
 
 if __name__ == '__main__':
     unittest.main()
