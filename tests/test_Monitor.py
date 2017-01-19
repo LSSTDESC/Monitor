36cf24f10d1b4c09ecd3ce53b29f92eaf585fbcd
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-01-19 14:31:41 -0800

    Cleaned up test_Monitor now that it is passing.

--- tests/test_Monitor.py
+++ tests/test_Monitor.py
@@ -9,22 +9,11 @@ import sqlite3
 import desc.monitor
 
 class MonitorTestCase(unittest.TestCase):
-    # def setUp(self):
-    #     self.test_db = 'test_cache.db'
-    #     self.dbConn = desc.monitor.dbInterface(database=self.test_db,
-    #                                            host=None, port=None,
-    #                                            driver='sqlite')
-    #     self.monitor = desc.monitor.Monitor(self.dbConn)
-    #     self.photfile = 'light_curve.txt'
-    #     subprocess.call('touch %s' % self.photfile, shell=True)
-    # def tearDown(self):
-    #     os.remove(self.photfile)
-    #     del self.monitor
 
     def setUp(self):
         """
         Create a test dataset as an sqlite db for exercising
-        get_twinkles_vists.
+        monitor class.
         """
         self._test_file = 'my_test_sqlite.db'
         connection = sqlite3.connect(self._test_file)
@@ -52,6 +41,9 @@ class MonitorTestCase(unittest.TestCase):
         os.remove(self._test_file)
 
     def test_db_load(self):
+        """
+        Simple test that monitor loads with active dbConnection.
+        """
         self.assertEqual(self.monitor.num_visits, 5)
 
 if __name__ == '__main__':
