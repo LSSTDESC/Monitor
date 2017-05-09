cebfa220b03a92600704a3796b6fc95ec09554fd
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-05-09 16:18:49 -0700

    Finished commenting. Added demo for creating truth database.

--- python/desc/monitor/CreateTruthDB.py
+++ python/desc/monitor/CreateTruthDB.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import, division, print_function
-import os
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
-from lsst.utils import getPackageDir
 from lsst.sims.catalogs.db import CatalogDBObject
 from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
 from lsst.sims.catUtils.exampleCatalogDefinitions import DefaultPhoSimHeaderMap
@@ -14,7 +12,11 @@ from desc.monitor.TruthCatalogDefs import TruthCatalogPoint
 
 __all__ = ["StarCacheDBObj", "TrueStars"]
 
+
 class StarCacheDBObj(CatalogDBObject):
+    """
+    CatalogDBObject for the stars in the simulation "truth" database.
+    """
     tableid = 'star_cache_table'
     host = None
     port = None
@@ -24,7 +26,7 @@ class StarCacheDBObj(CatalogDBObject):
     raColName = 'ra'
     decColName = 'decl'
 
-    columns = [('id','simobjid', int),
+    columns = [('id', 'simobjid', int),
                ('raJ2000', 'ra*PI()/180.'),
                ('decJ2000', 'decl*PI()/180.'),
                ('glon', 'gal_l*PI()/180.'),
@@ -37,28 +39,49 @@ class StarCacheDBObj(CatalogDBObject):
                ('variabilityParameters', 'varParamStr', str, 256),
                ('sedFilename', 'sedfilename', str, 256)]
 
+
 class TrueStars(object):
+    """
+    Gets the stars out of the simulation "truth" database for a specified set
+    of visits.
+
+    Parameters
+    ----------
+    dbConn : dbInterface instance
+        This is a connection to a cached database of the simulation inputs.
+
+    opsimDB_filename : str
+        The location of the opsim database used with the simulation.
+    """
 
     def __init__(self, dbConn, opsimDB_filename):
 
         self.dbConn = dbConn
         self.opsimDB = opsimDB_filename
         # Set up OpSim database (from Twinkles/bin/generatePhosimInput.py)
-        engine = create_engine('sqlite:///' + self.opsimDB)
         self.obs_gen = ObservationMetaDataGenerator(database=self.opsimDB,
                                                     driver='sqlite')
 
     def get_true_stars(self, for_obsHistIds=None):
-
         """
-        Get all the fluxes for stars in all visits in Twinkles.
+        Get all the fluxes for stars in all visits specified.
 
-        Can specify a subset of visits with for_obsHistIds.
+        Parameters
+        ----------
+        for_obsHistIds : list of ints or None, default=None
+          Can specify a subset of visits. If set to None will get it from the
+          file in the repo located at monitor/data/selectedVisits.csv
+
+        Attributes
+        ----------
+        star_df : pandas dataframe
+          Stores all the star information for the simulation inputs across
+          the desired visits
         """
 
         if for_obsHistIds is None:
             survey_info = np.genfromtxt('../data/selectedVisits.csv',
-                                    names=True, delimiter=',')
+                                        names=True, delimiter=',')
             for_obsHistIds = survey_info['obsHistID']
 
         obs_metadata_list = []
@@ -74,9 +97,9 @@ class TrueStars(object):
                                                     fieldDec=(-29, -27),
                                                     boundLength=0.3)[0])
 
-        star_df = pd.DataFrame(columns = ['uniqueId', 'ra', 'dec', 'filter',
-                                          'true_flux', 'true_flux_error',
-                                          'obsHistId'])
+        star_df = pd.DataFrame(columns=['uniqueId', 'ra', 'dec', 'filter',
+                                        'true_flux', 'true_flux_error',
+                                        'obsHistId'])
         bp_dict = BandpassDict.loadTotalBandpassesFromFiles()
         bp_indices = {}
         for bp in list(enumerate(bp_dict.keys())):
@@ -91,7 +114,8 @@ class TrueStars(object):
                 print("Generated fluxes for %i out of %i visits" %
                       (visit_on+1, len(for_obsHistIds)))
             visit_on += 1
-            star_cat = TruthCatalogPoint(self.dbConn, obs_metadata=obs_metadata,
+            star_cat = TruthCatalogPoint(self.dbConn,
+                                         obs_metadata=obs_metadata,
                                          constraint='gmag > 11')
 
             if column_names is None:
@@ -102,12 +126,12 @@ class TrueStars(object):
                 chunk_data.append(line)
             chunk_data = pd.DataFrame(chunk_data, columns=column_names)
 
-            #All SEDs will be the same since we are looking at the same point
-            #in the sky and mag_norms will be the same for stars.
+            # All SEDs will be the same since we are looking at the same point
+            # in the sky and mag_norms will be the same for stars.
             if seds_loaded is False:
                 sed_list = SedList(chunk_data['sedFilepath'],
                                    chunk_data['phoSimMagNorm'],
-                                   specMap = None,
+                                   specMap=None,
                                    galacticAvList=chunk_data['galacticAv'])
                 seds_loaded = True
 
@@ -115,27 +139,52 @@ class TrueStars(object):
                 phot_params = PhotometricParameters()
 
             visit_filter = obs_metadata.OpsimMetaData['filter']
-            flux_array = np.power(10,-0.4*(mag_array[visit_filter] - 22.5))
+            flux_array = np.power(10, -0.4*(mag_array[visit_filter] - 22.5))
+            five_sigma_depth = obs_metadata.OpsimMetaData['fiveSigmaDepth']
             snr, gamma = calcSNR_m5(mag_array[visit_filter],
                                     bp_dict[visit_filter],
-                                    obs_metadata.OpsimMetaData['fiveSigmaDepth'],
+                                    five_sigma_depth,
                                     phot_params)
             flux_error = flux_array/snr
 
+            obs_hist_id = obs_metadata.OpsimMetaData['obsHistID']
             visit_df = pd.DataFrame(np.array([chunk_data['uniqueId'],
                                     chunk_data['raJ2000'],
                                     chunk_data['decJ2000'],
                                     [visit_filter]*len(chunk_data),
                                     flux_array, flux_error,
-                                    [obs_metadata.OpsimMetaData['obsHistID']]*len(chunk_data)]).T,
-                                    columns = ['uniqueId', 'ra', 'dec', 'filter',
-                                               'true_flux', 'true_flux_error',
-                                               'obsHistId'])
+                                    [obs_hist_id]*len(chunk_data)]).T,
+                                    columns=['uniqueId', 'ra', 'dec', 'filter',
+                                             'true_flux', 'true_flux_error',
+                                             'obsHistId'])
             star_df = star_df.append(visit_df, ignore_index=True)
+            star_df['uniqueId'] = star_df['uniqueId'].convert_objects(
+                                                          convert_numeric=True)
+            star_df['ra'] = star_df['ra'].convert_objects(convert_numeric=True)
+            star_df['dec'] = star_df['dec'].convert_objects(
+                                                          convert_numeric=True)
+            star_df['true_flux'] = star_df['true_flux'].convert_objects(
+                                                          convert_numeric=True)
+            t_f_error = 'true_flux_error'
+            star_df[t_f_error] = star_df[t_f_error].convert_objects(
+                                                          convert_numeric=True)
+            star_df['obsHistId'] = star_df['obsHistId'].convert_objects(
+                                                          convert_numeric=True)
 
         self.star_df = star_df
 
     def write_to_db(self, filename, table_name='stars'):
+        """
+        Write self.star_df to a sqlite database.
+
+        Parameters
+        ----------
+        filename : str
+          File name to use for the sqlite database.
+
+        table_name : str, default='stars'
+          Table name within the sqlite database for the star_df info.
+        """
 
         disk_engine = create_engine('sqlite:///%s' % filename)
-        worker.star_df.to_sql('stars', disk_engine)
+        self.star_df.to_sql('stars', disk_engine)
--- python/desc/monitor/dbConnection.py
+++ python/desc/monitor/dbConnection.py
@@ -4,7 +4,28 @@ from lsst.sims.catalogs.db import DBObject
 
 __all__ = ['dbInterface', 'truthDBInterface', 'opsimDBInterface']
 
+
 class dbInterface(object):
+    """
+    Tools designed to access the pserv database.
+
+    Parameters
+    ----------
+    database : str
+        Name of the database.
+
+    host : str
+        The location of the host.
+
+    port : int
+        Port to use for the connection.
+
+    driver : str
+        Type of database. Probably 'mysql'.
+
+    project : str, default="Twinkles Run1.1"
+        The specific project for which to return data from the pserv database.
+    """
 
     def __init__(self, database, host, port, driver,
                  project="Twinkles Run1.1"):
@@ -14,7 +35,19 @@ class dbInterface(object):
         self.project = project
 
     def forcedSourceFromId(self, objectId):
+        """
+        Get the flux information for all possible visits for a specific source.
 
+        Parameters
+        ----------
+        objectId : int
+            The object id of the source
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('ccd_visit_id', np.int),
                           ('psf_flux', np.float),
@@ -28,7 +61,20 @@ class dbInterface(object):
         return results
 
     def visitFromCcdVisitId(self, visitId):
+        """
+        Get the bandpass and mjd of a visit.
 
+        Parameters
+        ----------
+        visitId : int
+            The ccd visit id of the visit. Not the same as the obsHistId from
+            Opsim.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('visit_id', np.int),
                           ('filter', str, 300),
                           ('obs_start', str, 300)])
@@ -41,7 +87,20 @@ class dbInterface(object):
         return results
 
     def all_fs_visits_from_id(self, objectId):
+        """
+        Get the flux information as well as a join on the visit information
+        for all possible visits for a specific source.
 
+        Parameters
+        ----------
+        objectId : int
+            The object id of the source
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('ccd_visit_id', np.int),
                           ('psf_flux', np.float),
@@ -61,13 +120,25 @@ class dbInterface(object):
                        and ForcedSource.objectId = %i
                        and ForcedSource.project = '%s'
                        and CcdVisit.project = '%s'""" % (objectId,
-                                                           self.project,
-                                                           self.project)
+                                                         self.project,
+                                                         self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
     def objectFromId(self, objectId):
+        """
+        Get the location information and parent id for a specific source.
 
+        Parameters
+        ----------
+        objectId : int
+            The object id of the source
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('parent_object_id', np.int),
                           ('ra', np.float),
@@ -80,7 +151,26 @@ class dbInterface(object):
         return results
 
     def objectFromRaDec(self, ra, dec, tol):
+        """
+        Get the list of possible objects in a square box of 2*tol degrees
+        around the location (ra, dec) in degrees.
 
+        Parameters
+        ----------
+        ra : float
+            RA location in degrees
+
+        dec : float
+            DEC location in degrees
+
+        tol : float
+            radius of search in degrees
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('parent_object_id', np.int),
                           ('ra', np.float),
@@ -90,12 +180,19 @@ class dbInterface(object):
                    where (psRa > %f) and (psRa < %f)
                    and (psDecl > %f) and (psDecl < %f)
                    and project = '%s'""" % (ra - tol, ra + tol,
-                                          dec - tol, dec + tol, self.project)
+                                            dec - tol, dec + tol, self.project)
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
         return results
 
     def get_all_visit_info(self):
+        """
+        Get all the visit information for all possible visits in the project.
 
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('ccd_visit_id', np.int),
                           ('visit_id', np.int),
                           ('filter', str, 300),
@@ -112,7 +209,20 @@ class dbInterface(object):
         return results
 
     def get_all_objects_in_visit(self, ccd_visit_id):
+        """
+        Get the flux and location information for all sources in a visit.
 
+        Parameters
+        ----------
+        ccd_visit_id : int
+            The ccd visit id of the visit. Different from obsHistId from Opsim.
+            Can get the ccd visit id for visits using get_all_visit_info.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('ra', np.float),
                           ('dec', np.float),
@@ -137,7 +247,14 @@ class dbInterface(object):
         return results
 
     def get_number_of_visits(self):
+        """
+        Get the count on the ccdVisitId's in the project.
 
+        Returns
+        -------
+        results : int
+            Number of visits in the project.
+        """
         dtype = np.dtype([('count', np.int)])
 
         query = """select count(ccdVisitId)
@@ -149,7 +266,14 @@ class dbInterface(object):
         return results['count'][0]
 
     def get_object_locations(self):
+        """
+        Get the locations for all objects in a project.
 
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query to the pserv database.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('ra', np.float),
                           ('dec', np.float)])
@@ -161,7 +285,25 @@ class dbInterface(object):
 
         return results
 
+
 class truthDBInterface(object):
+    """
+    Tools designed to access the simulation "truth" database.
+
+    Parameters
+    ----------
+    database : str
+        Name of the database.
+
+    host : str or None, default=None
+        The location of the host.
+
+    port : int or None, default=None
+        Port to use for the connection.
+
+    driver : str, default='sqlite'
+        Type of database. Probably 'sqlite'.
+    """
 
     def __init__(self, database, host=None, port=None, driver='sqlite'):
 
@@ -169,6 +311,19 @@ class truthDBInterface(object):
                              driver=driver)
 
     def get_stars_by_visit(self, visit_num):
+        """
+        Get location and flux for all the stars for a visit.
+
+        Parameters
+        ----------
+        visit_num : int
+            The obsHistId of the visit.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query.
+        """
 
         dtype = np.dtype([('object_id', np.int),
                           ('ra', np.float),
@@ -184,6 +339,14 @@ class truthDBInterface(object):
         return results
 
     def get_all_star_ra_dec(self):
+        """
+        Get the location for all the stars in the database.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query.
+        """
 
         dtype = np.dtype([('object_id', np.int),
                           ('ra', np.float),
@@ -196,7 +359,19 @@ class truthDBInterface(object):
         return results
 
     def get_all_info_by_object_id(self, object_id_tuple):
+        """
+        Get location and flux for a tuple of object_ids.
 
+        Parameters
+        ----------
+        object_id_tuple : tuple of ints
+            The object_ids of the sources.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query.
+        """
         dtype = np.dtype([('object_id', np.int),
                           ('ra', np.float),
                           ('dec', np.float),
@@ -208,20 +383,51 @@ class truthDBInterface(object):
         query = str("""SELECT *
                    FROM stars
                    WHERE """ + ' or '.join(('unique_id = ' +
-                                         str(n) for n in object_id_tuple)))
+                                            str(n) for n in object_id_tuple)))
 
         results = self._dbo.execute_arbitrary(query, dtype=dtype)
 
         return results
 
-class opsimDBInterface(object):
 
+class opsimDBInterface(object):
+    """
+    Tools designed to access an LSST Opsim Database.
+
+    Parameters
+    ----------
+    database : str
+        Name of the database.
+
+    host : str or None, default=None
+        The location of the host.
+
+    port : int or None, default=None
+        Port to use for the connection.
+
+    driver : str, default='sqlite'
+        Type of database. Probably 'sqlite'.
+    """
     def __init__(self, database, host=None, port=None, driver='sqlite'):
 
         self._dbo = DBObject(database=database, host=host, port=port,
                              driver=driver)
 
     def get_summary_depth_info_for_visits(self, fieldID):
+        """
+        Get visit observing statistics information from Opsim database for all
+        visits to a specified field.
+
+        Parameters
+        ----------
+        fieldID : int
+            The id of the observation field.
+
+        Returns
+        -------
+        results : numpy recarray
+            Numpy recarray with the results of the query.
+        """
 
         dtype = np.dtype([('obsHistID', np.int),
                           ('FWHMeff', np.float),
